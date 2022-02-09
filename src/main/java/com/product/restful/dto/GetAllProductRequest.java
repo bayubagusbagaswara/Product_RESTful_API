818571bf8c3a83fb81d1124a4de604f6699ecb8d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-09 14:35:01 +0700

    add dto GetAllProductRequest

---
+++ src/main/java/com/product/restful/dto/GetAllProductRequest.java
@@ -0,0 +1,30 @@
+package com.product.restful.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllProductRequest {
+
+    @NotNull(message = "Page Index can not null")
+    @Min(value = 0, message = "Page Index must not be less than zero")
+    private Integer pageNo;
+
+    @NotNull(message = "Page Size can not null")
+    @Min(value = 1, message = "Page Size must not be less than one")
+    private Integer pageSize;
+
+    @NotBlank(message = "Property must not be blank")
+    private String sortBy;
+
+    @NotBlank(message = "Sort direction must be asc or desc")
+    private String sortDir;
+}
