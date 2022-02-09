18d4683bbc6729d12911bff9683c071663a0f3bd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-09 14:41:28 +0700

    add schema migrate database and sample data product

---
+++ src/main/resources/db/migration/V20211202__Create_Product_Schema.sql
@@ -0,0 +1,13 @@
+create table products (
+    id character varying(64) not null primary key default uuid_generate_v4(),
+    name character varying(64) not null,
+    price numeric(19,2) not null,
+    quantity integer not null,
+    description character varying(255) not null,
+    status_record character varying(255) not null,
+    created_at timestamp not null default now(),
+    created_by character varying(255) not null,
+    updated_at timestamp,
+    updated_by character varying(255)
+);
+
