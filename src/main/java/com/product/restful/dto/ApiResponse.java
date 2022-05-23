49d7572c15b322d44d107aee3c670217ae7d6ae5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 19:39:45 +0700

    added dto class ApiResponse

---
+++ src/main/java/com/product/restful/dto/ApiResponse.java
@@ -0,0 +1,39 @@
+package com.product.restful.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Builder
+@JsonPropertyOrder({
+        "success",
+        "message"
+})
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+
+    @JsonProperty("success")
+    private Boolean success;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonIgnore
+    private HttpStatus status;
+
+    public ApiResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+}
