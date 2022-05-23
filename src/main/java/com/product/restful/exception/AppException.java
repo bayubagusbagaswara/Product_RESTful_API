612a174f1e16950b02df64bc978e20bbc87c00c2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 19:52:05 +0700

    added exception class AppException

---
+++ src/main/java/com/product/restful/exception/AppException.java
@@ -0,0 +1,21 @@
+package com.product.restful.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class AppException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
