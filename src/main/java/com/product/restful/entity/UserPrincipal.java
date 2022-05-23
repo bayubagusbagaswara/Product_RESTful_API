e885e4b5359e7bf40ebed15a6d56bc72f6eb7f05
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 19:33:27 +0700

    updated class UserPrincipal

--- src/main/java/com/product/restful/entity/UserPrincipal.java
+++ src/main/java/com/product/restful/entity/UserPrincipal.java
@@ -1,7 +1,6 @@
 package com.product.restful.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
