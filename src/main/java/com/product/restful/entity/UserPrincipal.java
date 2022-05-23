317f7bf2f93846bee3e79f0a3cec4429f31cc886
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 19:17:02 +0700

    add class UserPrincipal

---
+++ src/main/java/com/product/restful/entity/UserPrincipal.java
@@ -0,0 +1,142 @@
+package com.product.restful.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * UserPrincipal ini harus implement UserDetails, karena untuk mengakses atau mengambil data User, seperti name, password, email, dan authority
+ * jadi setiap kita ingin mengambil data UserDetails saat ini, maka kita bisa gunakan UserPrincipal
+ */
+@Data
+@Builder
+@NoArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    @JsonIgnore
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipal(Long id,
+                         String firstName,
+                         String lastName,
+                         String username,
+                         String email,
+                         String password,
+                         Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+
+        if (authorities == null) {
+            this.authorities = null;
+        } else {
+            this.authorities = new ArrayList<>(authorities);
+        }
+    }
+
+    // method create UserPrincipal berdasarkan data User
+    public static UserPrincipal create(User user) {
+        // ambil data roles dari user
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> {
+                    return new SimpleGrantedAuthority(role.getName().getRoleName());
+                })
+                .collect(Collectors.toList())
+                ;
+
+        return UserPrincipal.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (authorities == null) return null;
+        return new ArrayList<>(authorities);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null || getClass() != object.getClass())
+            return false;
+        UserPrincipal that = (UserPrincipal) object;
+        return Objects.equals(id, that.id);
+    }
+
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
