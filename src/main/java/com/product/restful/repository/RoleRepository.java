effe32edb6ee6ac4500e5a5054460d6a674ce4f7
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 19:41:51 +0700

    added interface RoleRepository

---
+++ src/main/java/com/product/restful/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.product.restful.repository;
+
+import com.product.restful.entity.Role;
+import com.product.restful.entity.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName name);
+}
