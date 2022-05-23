9e997eacb9c0c778fde5d85b63f4ea3e22e4c057
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 16:19:17 +0700

    add entity Role

---
+++ src/main/java/com/product/restful/entity/Role.java
@@ -0,0 +1,27 @@
+package com.product.restful.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles", uniqueConstraints = { @UniqueConstraint(columnNames = "name")})
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name")
+    private RoleName name;
+
+    public Role(RoleName name) {
+        this.name = name;
+    }
+}
