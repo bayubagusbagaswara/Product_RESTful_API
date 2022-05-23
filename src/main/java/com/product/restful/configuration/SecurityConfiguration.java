8abeb90d7432273b398afa85f859d2c86512b03a
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 18:52:14 +0700

    add class SecurityConfiguration

---
+++ src/main/java/com/product/restful/configuration/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.product.restful.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2Y);
+    }
+
+}
