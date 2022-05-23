3d2defcac3d26ee245cdc81752f1b9f3ad533330
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-23 19:41:38 +0700

    added interface UserRepository

---
+++ src/main/java/com/product/restful/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package com.product.restful.repository;
+
+import com.product.restful.entity.User;
+import com.product.restful.entity.UserPrincipal;
+import com.product.restful.exception.ResourceNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    default User getUser(UserPrincipal currentUser) {
+        return getUserByName(currentUser.getUsername());
+    }
+
+    default User getUserByName(String username) {
+        return findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+    }
+
+}
