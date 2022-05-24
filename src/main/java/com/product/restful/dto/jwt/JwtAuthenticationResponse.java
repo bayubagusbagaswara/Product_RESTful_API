944b17c347cb9f4ec6c4d72a577af1eeeff1d031
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-24 19:21:10 +0700

    add dto JwtAuthenticationResponse

---
+++ src/main/java/com/product/restful/dto/jwt/JwtAuthenticationResponse.java
@@ -0,0 +1,21 @@
+package com.product.restful.dto.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtAuthenticationResponse {
+
+    private String accessToken;
+
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
