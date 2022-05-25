04085df904a180542c98e123931aaa0d15457076
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-25 08:37:18 +0700

    updated method handler registerUser

--- src/main/java/com/product/restful/controller/AuthController.java
+++ src/main/java/com/product/restful/controller/AuthController.java
@@ -4,15 +4,11 @@ import com.product.restful.dto.ApiResponse;
 import com.product.restful.dto.auth.LoginRequest;
 import com.product.restful.dto.auth.SignUpRequest;
 import com.product.restful.dto.jwt.JwtAuthenticationResponse;
-import com.product.restful.entity.Role;
-import com.product.restful.entity.RoleName;
 import com.product.restful.entity.User;
-import com.product.restful.exception.AppException;
-import com.product.restful.exception.BlogApiException;
 import com.product.restful.repository.RoleRepository;
 import com.product.restful.repository.UserRepository;
 import com.product.restful.security.JwtTokenProvider;
-import org.springframework.http.HttpStatus;
+import com.product.restful.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -27,7 +23,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.*;
 
 /**
  * AuthController akan menghandler endpoint signUp, signIn, dan refreshToken
@@ -36,20 +31,33 @@ import java.util.*;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private static final String USER_ROLE_NOT_SET = "User role not set";
-
+    private final AuthService authService;
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
+        this.authService = authService;
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+
+        final User user = authService.signUp(signUpRequest);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/{userId}")
+                .buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(new ApiResponse(Boolean.TRUE, "User registered successfully"));
     }
 
     @PostMapping("/signin")
@@ -69,45 +77,5 @@ public class AuthController {
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<ApiResponse> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
 
-        if (Boolean.TRUE.equals(userRepository.existsByUsername(signUpRequest.getUsername()))) {
-            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Username is already taken");
-        }
-
-        if (Boolean.TRUE.equals(userRepository.existsByEmail(signUpRequest.getEmail()))) {
-            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Email is already taken");
-        }
-
-        User user = User.builder()
-                .firstName(signUpRequest.getFirstName().toLowerCase())
-                .lastName(signUpRequest.getLastName().toLowerCase())
-                .username(signUpRequest.getUsername().toLowerCase())
-                .email(signUpRequest.getEmail().toLowerCase())
-                .password(passwordEncoder.encode(signUpRequest.getPassword()))
-                .build();
-
-        Set<Role> roles = new HashSet<>();
-
-        if (userRepository.count() == 0) {
-            roles.add(roleRepository.findByName(RoleName.MEMBER.getRoleName())
-                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
-            roles.add(roleRepository.findByName(RoleName.ADMIN.getRoleName())
-                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
-        } else {
-            roles.add(roleRepository.findByName(RoleName.MEMBER.getRoleName())
-                    .orElseThrow(() -> new AppException(USER_ROLE_NOT_SET)));
-        }
-
-        user.setRoles(roles);
-
-        User result = userRepository.save(user);
-
-        // arahkan ke url /api/users/{id}
-        URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/{userId}")
-                .buildAndExpand(result.getId()).toUri();
-
-        return ResponseEntity.created(location).body(new ApiResponse(Boolean.TRUE, "User registered successfully"));
-    }
 }
