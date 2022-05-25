78b4cfa0b782901f61871e69b4fc6932fb521bc5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-25 08:33:45 +0700

    update method contract signUp in AuthService

--- src/main/java/com/product/restful/service/AuthService.java
+++ src/main/java/com/product/restful/service/AuthService.java
@@ -1,8 +1,15 @@
 package com.product.restful.service;
 
+import com.product.restful.dto.auth.SignUpRequest;
+import com.product.restful.entity.User;
+
 public interface AuthService {
 
     // signup
+    // kalo di contoh reddit-clone, setelah signup maka kita akan kirim email verification
+    // untuk saat ini kita tidak ingin kirim verifikasi
+    // kita hanya ingin setelah signup, maka user akan menerima token untuk request selanjutnya
+    User signUp(SignUpRequest signUpRequest);
 
     // verifyAccount
 
