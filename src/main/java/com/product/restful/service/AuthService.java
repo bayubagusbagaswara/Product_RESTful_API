3acbe465f8457beada7713d33322bce86c22ea92
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-25 08:42:21 +0700

    add method contract service signIn in AuthService

--- src/main/java/com/product/restful/service/AuthService.java
+++ src/main/java/com/product/restful/service/AuthService.java
@@ -1,6 +1,8 @@
 package com.product.restful.service;
 
+import com.product.restful.dto.auth.LoginRequest;
 import com.product.restful.dto.auth.SignUpRequest;
+import com.product.restful.dto.jwt.JwtAuthenticationResponse;
 import com.product.restful.entity.User;
 
 public interface AuthService {
@@ -14,6 +16,8 @@ public interface AuthService {
     // verifyAccount
 
     // login
+    // kita hanya akan mengembalikan response token jika berhasil autentikasi user
+    JwtAuthenticationResponse signIn(LoginRequest loginRequest);
 
     // refresh token
 
