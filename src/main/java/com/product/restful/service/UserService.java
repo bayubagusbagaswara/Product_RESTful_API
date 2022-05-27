b40110dbab596e30c384e46817548055b2bf2312
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 19:07:10 +0700

    update method in UserService

--- src/main/java/com/product/restful/service/UserService.java
+++ src/main/java/com/product/restful/service/UserService.java
@@ -1,5 +1,6 @@
 package com.product.restful.service;
 
+import com.product.restful.dto.ApiResponse;
 import com.product.restful.dto.user.CreateUserRequest;
 import com.product.restful.dto.user.UpdateUserRequest;
 import com.product.restful.dto.user.UserResponse;
@@ -9,18 +10,13 @@ public interface UserService {
 
      UserResponse createUser(CreateUserRequest createUserRequest);
 
-    // user admin, dapat menambahkan role to User
      void addRoleToUser(String username, String roleName);
 
-     // get user by id
     UserResponse getUserById(Long id);
 
-    // menghapus user hanya bisa dilakukan oleh MANAGER
-    void deleteUser(String username, UserPrincipal currentUser);
+    ApiResponse deleteUser(String username, UserPrincipal currentUser);
 
-    // update user bisa dilakukan jika user berhasil terautentikasi (semua role bisa update)
-    // tapi tidak bisa mengupdate ROLE
     UserResponse updateUser(String username, UpdateUserRequest updateUserRequest, UserPrincipal currentUser);
 
-    // delete user
+    ApiResponse removeAdmin(String username);
 }
