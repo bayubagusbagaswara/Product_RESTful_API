48ccbd0f435698ca554dae729667a02c023ae17e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 19:06:39 +0700

    add exception AccessDeniedException

---
+++ src/main/java/com/product/restful/exception/AccessDeniedException.java
@@ -0,0 +1,48 @@
+package com.product.restful.exception;
+
+import com.product.restful.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class AccessDeniedException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private ApiResponse apiResponse;
+    private String message;
+
+    public AccessDeniedException(ApiResponse apiResponse) {
+        super();
+        this.apiResponse = apiResponse;
+    }
+
+    public AccessDeniedException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    public AccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ApiResponse getApiResponse() {
+        return apiResponse;
+    }
+
+    public void setApiResponse(ApiResponse apiResponse) {
+        this.apiResponse = apiResponse;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
