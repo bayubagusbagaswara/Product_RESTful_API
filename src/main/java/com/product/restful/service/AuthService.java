dec25f22331c99670519ef4bd2f9447235fa2598
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 09:56:00 +0700

    update AuthService

--- src/main/java/com/product/restful/service/AuthService.java
+++ src/main/java/com/product/restful/service/AuthService.java
@@ -1,24 +1,15 @@
 package com.product.restful.service;
 
+import com.product.restful.dto.auth.AuthenticationResponse;
 import com.product.restful.dto.auth.LoginRequest;
 import com.product.restful.dto.auth.SignUpRequest;
-import com.product.restful.dto.jwt.JwtAuthenticationResponse;
 import com.product.restful.entity.User;
 
 public interface AuthService {
 
-    // signup
-    // kalo di contoh reddit-clone, setelah signup maka kita akan kirim email verification
-    // untuk saat ini kita tidak ingin kirim verifikasi
-    // kita hanya ingin setelah signup, maka user akan menerima token untuk request selanjutnya
     User signUp(SignUpRequest signUpRequest);
 
-    // verifyAccount
-    // ini membutuhkan verificationToken
-
-    // login
-    // kita hanya akan mengembalikan response token jika berhasil autentikasi user
-    JwtAuthenticationResponse signIn(LoginRequest loginRequest);
+    AuthenticationResponse signIn(LoginRequest loginRequest);
 
     // refresh token
 
