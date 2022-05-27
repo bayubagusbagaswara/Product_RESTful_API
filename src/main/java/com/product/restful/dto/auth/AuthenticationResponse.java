6a8164ba60b01a64817fd046d059ac096c026731
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 09:55:30 +0700

    added dto AuthenticationResponse

---
+++ src/main/java/com/product/restful/dto/auth/AuthenticationResponse.java
@@ -0,0 +1,20 @@
+package com.product.restful.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private Instant expiresAt;
+    private String username;
+}
