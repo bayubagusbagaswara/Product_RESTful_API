ec874ebd65782b0345e603f17e6b4f6802f3c637
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 14:19:35 +0700

    update AuthController

--- src/main/java/com/product/restful/controller/AuthController.java
+++ src/main/java/com/product/restful/controller/AuthController.java
@@ -1,21 +1,13 @@
 package com.product.restful.controller;
 
 import com.product.restful.dto.ApiResponse;
+import com.product.restful.dto.auth.AuthenticationResponse;
 import com.product.restful.dto.auth.LoginRequest;
 import com.product.restful.dto.auth.SignUpRequest;
-import com.product.restful.dto.jwt.JwtAuthenticationResponse;
 import com.product.restful.entity.User;
-import com.product.restful.repository.RoleRepository;
-import com.product.restful.repository.UserRepository;
-import com.product.restful.security.JwtTokenProvider;
 import com.product.restful.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,27 +17,14 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 
-/**
- * AuthController akan menghandler endpoint signUp, signIn, dan refreshToken
- */
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService authService;
-    private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthController(AuthService authService, AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
+    public AuthController(AuthService authService) {
         this.authService = authService;
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/signup")
@@ -62,12 +41,12 @@ public class AuthController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<JwtAuthenticationResponse> authenticateUser(
+    public ResponseEntity<AuthenticationResponse> authenticateUser(
             @Valid @RequestBody LoginRequest loginRequest) {
 
-        final JwtAuthenticationResponse jwtAuthenticationResponse = authService.signIn(loginRequest);
+        AuthenticationResponse authenticationResponse = authService.signIn(loginRequest);
 
-        return new ResponseEntity<>(jwtAuthenticationResponse, HttpStatus.OK);
+        return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
     }
 
 
