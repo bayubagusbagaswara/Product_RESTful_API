471583bc595cabdeb5451633486f7190d29a7fe8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 08:38:45 +0700

    added returning object SpringSecurityAuditAwareImpl

--- src/main/java/com/product/restful/configuration/JpaAuditingConfiguration.java
+++ src/main/java/com/product/restful/configuration/JpaAuditingConfiguration.java
@@ -5,14 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
-
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditingConfiguration {
 
     @Bean
     public AuditorAware<String> auditorProvider() {
-        return () -> Optional.of("Current User");
+        return new SpringSecurityAuditAwareImpl();
     }
 }
