845dc45fd62d732950a643ac2bf0cfc1a93595e5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 17:14:07 +0700

    update schema migrate Products

--- src/main/resources/db/migration/V20211202__Products_Schema.sql
+++ src/main/resources/db/migration/V20211202__Products_Schema.sql
@@ -1,13 +1,12 @@
 create table products (
     id character varying(64) not null primary key default uuid_generate_v4(),
-    created_at timestamp not null default now(),
-    created_by character varying(255) not null,
+    created_at timestamp without time zone not null default now(),
     status_record character varying(255) not null,
-    updated_at timestamp,
+    updated_at timestamp without time zone not null,
+    created_by character varying(255),
     updated_by character varying(255),
-    name character varying(64) not null,
+    description character varying(255) not null,
+    name character varying(100) not null,
     price numeric(19,2) not null,
-    quantity integer not null,
-    description character varying(255) not null
+    quantity integer not null
 );
-
