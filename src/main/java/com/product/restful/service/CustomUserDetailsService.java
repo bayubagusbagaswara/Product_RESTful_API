7a088766af0382ebc7e9038e28a2bcd7a146baa9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 14:14:02 +0700

    update service CustomUserDetailsService

--- src/main/java/com/product/restful/service/CustomUserDetailsService.java
+++ src/main/java/com/product/restful/service/CustomUserDetailsService.java
@@ -4,6 +4,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface CustomUserDetailsService extends UserDetailsService {
-
     UserDetails loadUserById(Long id);
 }
