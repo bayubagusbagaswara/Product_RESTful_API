3db8a27582f23ac0064a5294db0dd24c8ef65029
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 18:44:11 +0700

    add dto LogoutRequest

---
+++ src/main/java/com/product/restful/dto/auth/LogoutRequest.java
@@ -0,0 +1,18 @@
+package com.product.restful.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogoutRequest {
+
+    @NotBlank
+    private String refreshToken;
+}
