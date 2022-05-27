55f44fce67cc00adaf073a2020c69c9c4f380758
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 13:58:54 +0700

    update UserDateAudit

--- src/main/java/com/product/restful/entity/audit/UserDateAudit.java
+++ src/main/java/com/product/restful/entity/audit/UserDateAudit.java
@@ -1,15 +1,12 @@
 package com.product.restful.entity.audit;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.product.restful.entity.StatusRecord;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 
 import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.MappedSuperclass;
 import java.io.Serial;
 
@@ -32,7 +29,4 @@ public abstract class UserDateAudit extends DateAudit {
     @LastModifiedBy
     private String updatedBy;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status_record", nullable = false)
-    private StatusRecord statusRecord = StatusRecord.ACTIVE;
 }
