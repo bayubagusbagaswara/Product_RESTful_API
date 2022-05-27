cc2798ed1817029248ea951ada16d48462b8346d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 13:40:11 +0700

    update entity Product

--- src/main/java/com/product/restful/entity/Product.java
+++ src/main/java/com/product/restful/entity/Product.java
@@ -1,25 +1,38 @@
 package com.product.restful.entity;
 
-import lombok.*;
+import com.product.restful.entity.audit.UserDateAudit;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "products")
-@Getter
-@Setter
-@ToString
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @SQLDelete(sql = "UPDATE products SET status_record = 'INACTIVE' WHERE id = ?")
 @Where(clause = "status_record = 'ACTIVE'")
-public class Product extends BaseEntity {
+public class Product extends UserDateAudit {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    @Column(name = "id", nullable = false, length = 64)
+    private String id;
 
     @Column(name = "name", nullable = false, length = 100)
     private String name;
