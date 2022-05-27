fa6ddc9991904a1656a97a04624643ab399e6cd9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 14:15:25 +0700

    update service implementation RefreshTokenServiceImpl

--- src/main/java/com/product/restful/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/product/restful/service/impl/RefreshTokenServiceImpl.java
@@ -1,14 +1,21 @@
 package com.product.restful.service.impl;
 
 import com.product.restful.entity.RefreshToken;
+import com.product.restful.repository.RefreshTokenRepository;
 import com.product.restful.service.RefreshTokenService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RefreshTokenServiceImpl implements RefreshTokenService {
 
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshTokenServiceImpl(RefreshTokenRepository refreshTokenRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
     @Override
-    public RefreshToken findByToken(String token) {
+    public RefreshToken findByRefreshToken(String token) {
         return null;
     }
 
