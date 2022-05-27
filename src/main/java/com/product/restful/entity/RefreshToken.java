83a316d14e75fed0068cb91afed8b02cc5c2a4e2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 14:01:50 +0700

    update entity RefreshToken

--- src/main/java/com/product/restful/entity/RefreshToken.java
+++ src/main/java/com/product/restful/entity/RefreshToken.java
@@ -1,20 +1,35 @@
 package com.product.restful.entity;
 
-import lombok.*;
+import com.product.restful.entity.audit.UserDateAudit;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.time.Instant;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "refresh_tokens", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "token", name = "refresh_tokens_token_unique")
+        @UniqueConstraint(columnNames = "refresh_token", name = "refresh_tokens_refresh_token_unique")
 })
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class RefreshToken extends BaseEntity {
+@SQLDelete(sql = "UPDATE refresh_tokens SET status_record = 'INACTIVE' WHERE id = ?")
+@Where(clause = "status_record = 'ACTIVE'")
+public class RefreshToken extends UserDateAudit {
 
     @OneToOne
     @JoinColumn(name = "id_user", foreignKey = @ForeignKey(name = "fk_refresh_token_user_id"), referencedColumnName = "id")
