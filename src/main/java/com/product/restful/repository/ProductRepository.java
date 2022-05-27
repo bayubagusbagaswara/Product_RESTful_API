2c66f338bc9a98d6fa90d6190c9245a554a3ad39
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-27 14:07:12 +0700

    update ProductRepository

--- src/main/java/com/product/restful/repository/ProductRepository.java
+++ src/main/java/com/product/restful/repository/ProductRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 
     Optional<Product> findByNameIgnoreCase(String name);
