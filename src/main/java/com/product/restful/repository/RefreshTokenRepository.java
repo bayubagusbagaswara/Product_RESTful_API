9cd26015b152557dd5ad0b94fbc31aba2762f5cf
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-28 10:25:36 +0700

    update repository

--- src/main/java/com/product/restful/repository/RefreshTokenRepository.java
+++ src/main/java/com/product/restful/repository/RefreshTokenRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findByRefreshToken(String token);
 
--- src/main/java/com/product/restful/repository/RoleRepository.java
+++ src/main/java/com/product/restful/repository/RoleRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, String> {
 
     Optional<Role> findByName(RoleName roleName);
 }
