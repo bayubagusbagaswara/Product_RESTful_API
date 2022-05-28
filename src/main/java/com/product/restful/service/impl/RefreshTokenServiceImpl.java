5f9e5548c33dca1f1a842cbf96ef14054c8e2dd7
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-28 18:15:20 +0700

    update logic method generateRefreshToken in RefreshTokenServiceImpl

--- src/main/java/com/product/restful/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/product/restful/service/impl/RefreshTokenServiceImpl.java
@@ -1,9 +1,14 @@
 package com.product.restful.service.impl;
 
+import com.product.restful.dto.refreshToken.RefreshTokenResponse;
 import com.product.restful.entity.RefreshToken;
+import com.product.restful.entity.User;
 import com.product.restful.exception.RefreshTokenNotFoundException;
+import com.product.restful.exception.ResourceNotFoundException;
 import com.product.restful.repository.RefreshTokenRepository;
+import com.product.restful.repository.UserRepository;
 import com.product.restful.service.RefreshTokenService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -12,19 +17,31 @@ import java.util.UUID;
 @Service
 public class RefreshTokenServiceImpl implements RefreshTokenService {
 
+    @Value("${app.refreshTokenExpired}")
+    private Long refreshTokenDurationMs;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
 
-    public RefreshTokenServiceImpl(RefreshTokenRepository refreshTokenRepository) {
+    public RefreshTokenServiceImpl(RefreshTokenRepository refreshTokenRepository, UserRepository userRepository) {
         this.refreshTokenRepository = refreshTokenRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
-    public RefreshToken generateRefreshToken() {
+    public RefreshTokenResponse generateRefreshToken(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+
+        Instant dateNow = Instant.now();
+
         RefreshToken refreshToken = new RefreshToken();
         refreshToken.setRefreshToken(UUID.randomUUID().toString());
-        refreshToken.setCreatedAt(Instant.now());
+        refreshToken.setCreatedAt(dateNow);
+        refreshToken.setExpiryDate(dateNow.plusMillis(refreshTokenDurationMs));
+        refreshToken.setUser(user);
 
-        return refreshTokenRepository.save(refreshToken);
+        return RefreshTokenResponse.mapToDto(refreshToken);
     }
 
     @Override
@@ -37,4 +54,5 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
     public void deleteRefreshToken(String token) {
         refreshTokenRepository.deleteByRefreshToken(token);
     }
+
 }
