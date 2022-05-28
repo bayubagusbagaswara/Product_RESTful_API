9cc0fba063fac04c744645b4330ccc3813fb0336
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-28 13:01:40 +0700

    update class entity Role

--- src/main/java/com/product/restful/entity/Role.java
+++ src/main/java/com/product/restful/entity/Role.java
@@ -1,29 +1,25 @@
 package com.product.restful.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.product.restful.entity.audit.UserDateAudit;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
-import java.io.Serial;
-import java.util.HashSet;
-import java.util.Set;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "roles", uniqueConstraints = { @UniqueConstraint(columnNames = "name", name = "roles_name_unique")})
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @SQLDelete(sql = "UPDATE roles SET status_record = 'INACTIVE' WHERE id = ?")
 @Where(clause = "status_record = 'ACTIVE'")
 public class Role extends UserDateAudit {
-
-//    @Serial
-//    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(generator = "system-uuid")
@@ -35,27 +31,4 @@ public class Role extends UserDateAudit {
     @Column(name = "name", length = 20, nullable = false)
     private RoleName name;
 
-//    @ManyToMany(mappedBy = "roles")
-//    @JsonIgnore
-//    private Set<User> users = new HashSet<>();
-
-    public Role(RoleName name) {
-        this.name = name;
-    }
-
-//    public void addUser(User user) {
-//        this.users.add(user);
-//        user.getRoles().add(this);
-//    }
-//
-//    public void removeUser(User user) {
-//        this.getUsers().remove(user);
-//        user.getRoles().remove(this);
-//    }
-//
-//    public void removeUsers() {
-//        for (User user : new HashSet<>(users)) {
-//            removeUser(user);
-//        }
-//    }
 }
