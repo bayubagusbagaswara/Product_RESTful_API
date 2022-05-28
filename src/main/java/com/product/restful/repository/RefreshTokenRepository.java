88d6753fb134f3afb69429d03dfe6ff0e3cc226c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-28 21:57:44 +0700

    update RefreshTokenRepository

--- src/main/java/com/product/restful/repository/RefreshTokenRepository.java
+++ src/main/java/com/product/restful/repository/RefreshTokenRepository.java
@@ -1,6 +1,7 @@
 package com.product.restful.repository;
 
 import com.product.restful.entity.RefreshToken;
+import com.product.restful.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,9 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByRefreshToken(String token);
 
+    Optional<RefreshToken> findByUser(User user);
+
     void deleteByRefreshToken(String token);
+
+    void deleteByUserId(Long userId);
 }
