683c94576605e4be6261178e96f4022a194d9f8b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-29 12:19:03 +0700

    add method contract service checkUsernameAvailability, checkEmailAvailability, getUserProfile, and getCurrentUser

--- src/main/java/com/product/restful/service/UserService.java
+++ src/main/java/com/product/restful/service/UserService.java
@@ -1,18 +1,23 @@
 package com.product.restful.service;
 
 import com.product.restful.dto.ApiResponse;
-import com.product.restful.dto.user.CreateUserRequest;
-import com.product.restful.dto.user.UpdateUserRequest;
-import com.product.restful.dto.user.UserResponse;
+import com.product.restful.dto.user.*;
 import com.product.restful.entity.RoleName;
 import com.product.restful.entity.UserPrincipal;
 
 public interface UserService {
 
-    UserResponse crateAdmin(CreateUserRequest createUserRequest);
-     UserResponse createUser(CreateUserRequest createUserRequest);
+    UserIdentityAvailability checkUsernameAvailability(String username);
+    UserIdentityAvailability checkEmailAvailability(String email);
 
-     void addRoleToUser(String username, RoleName roleName);
+    UserProfileResponse getUserProfile(String username);
+
+    UserSummaryResponse getCurrentUser(String username);
+
+    UserResponse crateAdmin(CreateUserRequest createUserRequest);
+    UserResponse createUser(CreateUserRequest createUserRequest);
+
+    void addRoleToUser(String username, RoleName roleName);
 
     UserResponse getUserById(Long id);
 
