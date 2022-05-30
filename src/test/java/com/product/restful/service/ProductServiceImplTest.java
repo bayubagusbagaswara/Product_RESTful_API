eeffd3ff3e95433585328ca398aad70ac9110ee4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-30 11:04:34 +0700

    update ProductServiceImplTest

--- src/test/java/com/product/restful/service/ProductServiceImplTest.java
+++ src/test/java/com/product/restful/service/ProductServiceImplTest.java
@@ -1,9 +1,7 @@
 package com.product.restful.service;
 
-import com.product.restful.dto.*;
 import com.product.restful.dto.product.CreateProductRequest;
 import com.product.restful.dto.product.ProductResponse;
-import com.product.restful.exception.ProductNotFoundException;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -14,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
