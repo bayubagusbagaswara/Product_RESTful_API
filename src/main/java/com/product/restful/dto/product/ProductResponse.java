3f1ea216b129bebec2acc32166b66ad73928f8dd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-30 11:01:31 +0700

    update dto ProductResponse

--- src/main/java/com/product/restful/dto/product/ProductResponse.java
+++ src/main/java/com/product/restful/dto/product/ProductResponse.java
@@ -1,5 +1,6 @@
 package com.product.restful.dto.product;
 
+import com.product.restful.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -28,7 +31,21 @@ public class ProductResponse {
 
     private Instant createdAt;
 
-    private String updatedBy;
+    public static ProductResponse fromProduct(Product product) {
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setId(product.getId());
+        productResponse.setName(product.getName());
+        productResponse.setPrice(product.getPrice());
+        productResponse.setQuantity(product.getQuantity());
+        productResponse.setDescription(product.getDescription());
+        productResponse.setCreatedBy(product.getCreatedBy());
+        productResponse.setCreatedAt(product.getCreatedAt());
+        return productResponse;
+    }
 
-    private Instant updatedAt;
+    public static List<ProductResponse> fromProductList(List<Product> productList) {
+        return productList.stream()
+                .map(ProductResponse::fromProduct)
+                .collect(Collectors.toList());
+    }
 }
