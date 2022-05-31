5142f3bb97960d7988ea44a72fe291bd7fc72b54
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-31 11:34:29 +0700

    add service RoleServiceImpl

---
+++ src/main/java/com/product/restful/service/impl/RoleServiceImpl.java
@@ -0,0 +1,50 @@
+package com.product.restful.service.impl;
+
+import com.product.restful.dto.ApiResponse;
+import com.product.restful.dto.role.RoleRequest;
+import com.product.restful.dto.role.RoleResponse;
+import com.product.restful.entity.Role;
+import com.product.restful.entity.RoleName;
+import com.product.restful.entity.UserPrincipal;
+import com.product.restful.exception.AccessDeniedException;
+import com.product.restful.exception.ResourceNotFoundException;
+import com.product.restful.repository.RoleRepository;
+import com.product.restful.service.RoleService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+@Service
+@Transactional
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public RoleResponse createRole(RoleRequest roleRequest) {
+        Role role = new Role();
+        role.setName(RoleName.valueOf(roleRequest.getName().toUpperCase()));
+        role.setCreatedAt(Instant.now());
+        roleRepository.save(role);
+        return RoleResponse.from(role);
+    }
+
+    @Override
+    public ApiResponse deleteRole(Long id, UserPrincipal currentUser) {
+        Role role = roleRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Role", "id", id));
+
+        if (!currentUser.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ADMIN.toString()))) {
+            throw new AccessDeniedException(new ApiResponse(Boolean.FALSE, "You don't have permission to delete role of: " + role.getName()));
+        }
+
+        roleRepository.delete(role);
+        return new ApiResponse(Boolean.TRUE, "You successfully deleted role id of: " + id);
+    }
+}
