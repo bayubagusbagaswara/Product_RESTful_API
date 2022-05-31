a4729fbfdb83cdc94ba8fe129c5d866c02c891b5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-31 17:53:02 +0700

    update controller ProductController

--- src/main/java/com/product/restful/controller/ProductController.java
+++ src/main/java/com/product/restful/controller/ProductController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -38,7 +39,8 @@ public class ProductController {
     @PostMapping(
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("hasRole('MEMBER')")
+    @PreAuthorize("hasRole('USER')")
+//    @RolesAllowed({"ADMIN", "CUSTOMER", "USER"})
     public ResponseEntity<WebResponse<ProductResponse>> createProduct(@RequestBody CreateProductRequest createProductRequest) {
         ProductResponse productResponse = productService.createProduct(createProductRequest);
         return new ResponseEntity<>(new WebResponse<>(Boolean.TRUE, "Product was created successfully", productResponse), HttpStatus.CREATED);
