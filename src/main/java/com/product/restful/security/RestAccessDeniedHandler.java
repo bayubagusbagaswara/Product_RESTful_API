da4ad35eebe41a0c57110bbf6515a7ec01a175e0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-05 09:43:27 +0700

    add class handler RestAccessDeniedHandler

---
+++ src/main/java/com/product/restful/security/RestAccessDeniedHandler.java
@@ -0,0 +1,35 @@
+package com.product.restful.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.product.restful.dto.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RestAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestAccessDeniedHandler.class);
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException accessDeniedException) throws IOException {
+
+        LOGGER.error("Responding with access denied error. Message - {}", accessDeniedException.getMessage());
+        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        ApiResponse body = new ApiResponse(Boolean.FALSE, "Access Denied. You don't have permission to access this resource", HttpStatus.FORBIDDEN);
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(httpServletResponse.getOutputStream(), body);
+    }
+}
