9b40615de58b5f765ef8b9bf4a2e0f1dfd786f68
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-15 17:01:39 +0700

    update method getByName in RoleRepository

--- src/main/java/com/product/restful/repository/RoleRepository.java
+++ src/main/java/com/product/restful/repository/RoleRepository.java
@@ -1,14 +1,16 @@
 package com.product.restful.repository;
 
 import com.product.restful.entity.Role;
-import com.product.restful.entity.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Optional<Role> findByName(RoleName roleName);
+    @Query("FROM Role r WHERE LOWER(r.name) = LOWER(:name)")
+    Optional<Role> getByName(@RequestParam(name = "name") String name);
 }
