46f72379d7f951800774492c97bfd428cd05ce18
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-15 16:14:56 +0700

    update UserController

--- src/main/java/com/product/restful/controller/UserController.java
+++ src/main/java/com/product/restful/controller/UserController.java
@@ -5,10 +5,8 @@ import com.product.restful.dto.WebResponse;
 import com.product.restful.dto.role.RoleRequest;
 import com.product.restful.dto.user.CreateUserRequest;
 import com.product.restful.dto.user.UpdateUserRequest;
-import com.product.restful.dto.user.UserResponse;
+import com.product.restful.dto.user.UserDto;
 import com.product.restful.entity.RoleName;
-import com.product.restful.entity.UserPrincipal;
-import com.product.restful.security.CurrentUser;
 import com.product.restful.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,39 +32,29 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<WebResponse<UserResponse>> createUser(@Valid @RequestBody CreateUserRequest user) {
-        final UserResponse userResponse = userService.createUser(user);
-        return new ResponseEntity<>(new WebResponse<>(Boolean.TRUE, "User was created successfully", userResponse), HttpStatus.CREATED);
+    public ResponseEntity<WebResponse<UserDto>> createUser(@Valid @RequestBody CreateUserRequest user) {
+        final UserDto userDto = userService.createUser(user);
+        return new ResponseEntity<>(new WebResponse<>(Boolean.TRUE, "User was created successfully", userDto), HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/{username}")
     @PreAuthorize(value = "hasAnyAuthority('ADMIN', 'USER')")
-    public ResponseEntity<WebResponse<UserResponse>> updateUser(
-            @Valid @RequestBody UpdateUserRequest newUser,
-            @PathVariable(name = "username") String username,
-            @CurrentUser UserPrincipal currentUser) {
-
-        final UserResponse userResponse = userService.updateUser(username, newUser, currentUser);
-        return new ResponseEntity<>(new WebResponse<>(Boolean.TRUE, "User updated successfully", userResponse), HttpStatus.OK);
+    public ResponseEntity<WebResponse<UserDto>> updateUser(@Valid @RequestBody UpdateUserRequest newUser, @PathVariable(name = "username") String username) {
+        final UserDto userDto = userService.updateUser(username, newUser);
+        return new ResponseEntity<>(new WebResponse<>(Boolean.TRUE, "User updated successfully", userDto), HttpStatus.OK);
     }
 
     @PutMapping(value = "/{username}/addRole")
     @PreAuthorize(value = "hasAuthority('ADMIN')")
-    public ResponseEntity<ApiResponse> addRoleToUser(
-            @PathVariable(name = "username") String username,
-            @RequestBody RoleRequest roleRequest) {
-
+    public ResponseEntity<ApiResponse> addRoleToUser(@PathVariable(name = "username") String username, @RequestBody RoleRequest roleRequest) {
         userService.addRoleToUser(username, RoleName.valueOf(roleRequest.getName().toUpperCase()));
-        return new ResponseEntity<>(new ApiResponse(Boolean.TRUE, "Successfully added role to user: " + username), HttpStatus.OK);
+        return new ResponseEntity<>(new ApiResponse(Boolean.TRUE, "Successfully added role " + roleRequest.getName().toUpperCase() + " to user: " + username), HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{username}")
     @PreAuthorize(value = "hasAuthority('ADMIN')")
-    public ResponseEntity<ApiResponse> deleteUser(
-            @PathVariable(name = "username") String username,
-            @CurrentUser UserPrincipal currentUser) {
-
-        ApiResponse apiResponse = userService.deleteUser(username, currentUser);
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable(name = "username") String username) {
+        ApiResponse apiResponse = userService.deleteUser(username);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
