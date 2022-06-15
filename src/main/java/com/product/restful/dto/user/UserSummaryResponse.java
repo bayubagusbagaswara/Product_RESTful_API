048858ac528bf0824f22ab121fa58195045395fa
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-15 16:27:07 +0700

    update dto UserSummaryResponse

--- src/main/java/com/product/restful/dto/user/UserSummaryResponse.java
+++ src/main/java/com/product/restful/dto/user/UserSummaryResponse.java
@@ -1,12 +1,10 @@
 package com.product.restful.dto.user;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserSummaryResponse {
