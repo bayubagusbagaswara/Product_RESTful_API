5c3602e177edd6e0129efe8ad16461851cc02434
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-15 16:14:08 +0700

    update RefreshTokenResponse

--- src/main/java/com/product/restful/dto/refreshToken/RefreshTokenResponse.java
+++ src/main/java/com/product/restful/dto/refreshToken/RefreshTokenResponse.java
@@ -1,43 +1,28 @@
 package com.product.restful.dto.refreshToken;
 
-import com.product.restful.dto.user.UserResponse;
+import com.product.restful.dto.user.UserDto;
 import com.product.restful.entity.RefreshToken;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RefreshTokenResponse {
 
     private Long id;
 
-    private UserResponse user;
+    private UserDto user;
 
     private String refreshToken;
 
     private Instant expiryDate;
 
     public static RefreshTokenResponse mapToDto(RefreshToken refreshToken) {
-        return RefreshTokenResponse.builder()
-                .id(refreshToken.getId())
-                .user(UserResponse.fromUser(refreshToken.getUser()))
-                .refreshToken(refreshToken.getRefreshToken())
-                .expiryDate(refreshToken.getExpiryDate())
-                .build();
+        return new RefreshTokenResponse(refreshToken.getId(), UserDto.fromUser(refreshToken.getUser()), refreshToken.getRefreshToken(), refreshToken.getExpiryDate());
     }
 
-    public static RefreshToken mapToEntity(RefreshTokenResponse refreshTokenResponse) {
-        return RefreshToken.builder()
-                .id(refreshTokenResponse.getId())
-                .user(UserResponse.fromUserResponse(refreshTokenResponse.getUser()))
-                .refreshToken(refreshTokenResponse.getRefreshToken())
-                .expiryDate(refreshTokenResponse.getExpiryDate())
-                .build();
-    }
 }
