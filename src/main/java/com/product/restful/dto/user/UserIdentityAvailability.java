8f38b5e357513ab8c4b2605db3f67b9faf36717c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-15 16:26:09 +0700

    update dto UserIdentityAvailability

--- src/main/java/com/product/restful/dto/user/UserIdentityAvailability.java
+++ src/main/java/com/product/restful/dto/user/UserIdentityAvailability.java
@@ -1,11 +1,9 @@
 package com.product.restful.dto.user;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 @AllArgsConstructor
 public class UserIdentityAvailability {
 
