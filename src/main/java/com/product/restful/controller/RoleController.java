0351681af683c9c54ae41f8279530ec8abbe39e9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-15 16:14:20 +0700

    update RoleController

--- src/main/java/com/product/restful/controller/RoleController.java
+++ src/main/java/com/product/restful/controller/RoleController.java
@@ -3,11 +3,9 @@ package com.product.restful.controller;
 import com.product.restful.dto.ApiResponse;
 import com.product.restful.dto.WebResponse;
 import com.product.restful.dto.role.RoleRequest;
-import com.product.restful.dto.role.RoleResponse;
+import com.product.restful.dto.role.RoleDto;
 import com.product.restful.entity.UserPrincipal;
-import com.product.restful.security.CurrentUser;
 import com.product.restful.service.RoleService;
-import org.apache.catalina.WebResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +22,13 @@ public class RoleController {
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<WebResponse<RoleResponse>> createRole(@RequestBody RoleRequest roleRequest) {
-        RoleResponse roleResponse = roleService.createRole(roleRequest);
-        return new ResponseEntity<>(new WebResponse<>(Boolean.TRUE, "Role was created successfully", roleResponse), HttpStatus.OK);
+    public ResponseEntity<WebResponse<RoleDto>> createRole(@RequestBody RoleRequest roleRequest) {
+        RoleDto roleDto = roleService.createRole(roleRequest);
+        return new ResponseEntity<>(new WebResponse<>(Boolean.TRUE, "Role was created successfully", roleDto), HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ApiResponse> deleteRole(@PathVariable(name = "id") Long id, @CurrentUser UserPrincipal currentUser) {
+    public ResponseEntity<ApiResponse> deleteRole(@PathVariable(name = "id") Long id, UserPrincipal currentUser) {
         ApiResponse apiResponse = roleService.deleteRole(id, currentUser);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
