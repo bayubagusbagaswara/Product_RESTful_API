8fa8fdb0c60c86ceee23662801b35f155b9ea88e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-16 20:12:38 +0700

    update method in RefreshTokenRepository

--- src/main/java/com/product/restful/repository/RefreshTokenRepository.java
+++ src/main/java/com/product/restful/repository/RefreshTokenRepository.java
@@ -3,18 +3,23 @@ package com.product.restful.repository;
 import com.product.restful.entity.RefreshToken;
 import com.product.restful.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
-    Optional<RefreshToken> findByRefreshToken(String token);
+    @Query("FROM RefreshToken r WHERE r.refreshToken = :token")
+    Optional<RefreshToken> getByRefreshToken(@RequestParam(name = "token") String token);
 
-    Optional<RefreshToken> findByUser(User user);
+    @Query("FROM RefreshToken r WHERE r.user = :user")
+    Optional<RefreshToken> getByUser(@RequestParam(name = "user") User user);
 
-    void deleteByRefreshToken(String token);
-
-    void deleteByUser(User user);
+//    @Modifying
+//    @Query("DELETE FROM RefreshToken r WHERE r.user = :user")
+    void deleteByUserId(Long userId);
 }
