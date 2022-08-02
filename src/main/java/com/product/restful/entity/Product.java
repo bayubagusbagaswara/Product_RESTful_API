e82f0dbf7287b4467f67b2f23278c20aa8dc7911
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-02 08:49:11 +0700

    update entity Product

--- src/main/java/com/product/restful/entity/Product.java
+++ src/main/java/com/product/restful/entity/Product.java
@@ -1,6 +1,7 @@
 package com.product.restful.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.product.restful.entity.audit.UserDateAudit;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.Instant;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -21,6 +23,9 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+//@JsonIgnoreProperties(
+//        value = { "updatedAt", "updatedBy"}
+//)
 @SQLDelete(sql = "UPDATE products SET status_record = 'INACTIVE' WHERE id = ?")
 @Where(clause = "status_record = 'ACTIVE'")
 public class Product extends UserDateAudit {
@@ -47,4 +52,27 @@ public class Product extends UserDateAudit {
     @Enumerated(EnumType.STRING)
     @Column(name = "status_record", nullable = false)
     private StatusRecord statusRecord = StatusRecord.ACTIVE;
+
+//    @JsonIgnore
+//    @Override
+//    public Instant getCreatedAt() {
+//        return super.getCreatedAt();
+//    }
+//
+//    @JsonIgnore
+//    @Override
+//    public Instant getUpdatedAt() {
+//        return super.getUpdatedAt();
+//    }
+//
+
+//    @Override
+//    public String getCreatedBy() {
+//        return super.getCreatedBy();
+//    }
+//
+//    @Override
+//    public String getUpdatedBy() {
+//        return super.getUpdatedBy();
+//    }
 }
