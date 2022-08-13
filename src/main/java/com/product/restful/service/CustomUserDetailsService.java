62875903922ad434baf8ede54cd7abfc18c494d0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-13 18:08:31 +0700

    override method loadUserByUsername

--- src/main/java/com/product/restful/service/CustomUserDetailsService.java
+++ src/main/java/com/product/restful/service/CustomUserDetailsService.java
@@ -2,7 +2,12 @@ package com.product.restful.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface CustomUserDetailsService extends UserDetailsService {
+
     UserDetails loadUserById(Long id);
+
+    @Override
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
