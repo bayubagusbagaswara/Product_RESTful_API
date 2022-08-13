1dce41875ca9e304d4a22ff6c0522e805c7bba60
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-13 13:22:33 +0700

    add method findByUniqueCode and deleteByUser

--- src/main/java/com/product/restful/repository/ResetPasswordRepository.java
+++ src/main/java/com/product/restful/repository/ResetPasswordRepository.java
@@ -1,9 +1,16 @@
 package com.product.restful.repository;
 
 import com.product.restful.entity.user.ResetPassword;
+import com.product.restful.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ResetPasswordRepository extends CrudRepository<ResetPassword, Long> {
+
+    Optional<ResetPassword> findByUniqueCode(String uniqueCode);
+
+    void deleteByUser(User user);
 }
