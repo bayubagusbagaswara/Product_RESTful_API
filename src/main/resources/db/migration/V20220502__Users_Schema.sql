845ea3c47a0757c9f51ae5b16b00c80ad9fa065e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-13 20:30:05 +0700

    update schema users

--- src/main/resources/db/migration/V20220502__Users_Schema.sql
+++ src/main/resources/db/migration/V20220502__Users_Schema.sql
@@ -4,6 +4,7 @@ create table users (
     created_at timestamp without time zone not null default now(),
     status_record character varying(255) not null,
     updated_at timestamp without time zone not null,
+    user_active boolean not null,
     email character varying(40),
     first_name character varying(40) not null,
     last_name character varying(40) not null,
@@ -20,7 +21,7 @@ alter table users
 
 create table user_password(
     id_user bigserial,
-    password character varying(255) not null,
+    password character varying(255),
     primary key (id_user),
     foreign key (id_user) references users(id)
 );
