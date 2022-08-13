c2ef82f5193b00c686232e2e92caefc635806998
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-13 20:31:21 +0700

    update UserRepository

--- src/main/java/com/product/restful/repository/UserRepository.java
+++ src/main/java/com/product/restful/repository/UserRepository.java
@@ -4,12 +4,13 @@ import com.product.restful.entity.user.User;
 import com.product.restful.entity.user.UserPrincipal;
 import com.product.restful.exception.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
