998be290954e6e3a35ab77c8153dfc06e0126480
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-13 18:13:47 +0700

    update method findByUser

--- src/main/java/com/product/restful/repository/UserPasswordRepository.java
+++ src/main/java/com/product/restful/repository/UserPasswordRepository.java
@@ -1,11 +1,11 @@
 package com.product.restful.repository;
 
+import com.product.restful.entity.user.User;
 import com.product.restful.entity.user.UserPassword;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserPasswordRepository extends CrudRepository<UserPassword, Long> {
-
-    UserPassword findByUserId(Long userId);
+    UserPassword findByUser(User user);
 }
