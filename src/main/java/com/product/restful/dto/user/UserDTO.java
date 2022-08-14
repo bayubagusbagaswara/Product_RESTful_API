41de1eb6f523b9e6c7a7952d6cfe5f3c329ba8e0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-14 14:24:27 +0700

    update UserDTO

--- src/main/java/com/product/restful/dto/user/UserDTO.java
+++ src/main/java/com/product/restful/dto/user/UserDTO.java
@@ -18,25 +18,19 @@ import java.util.stream.Collectors;
 public class UserDTO {
 
     private Long id;
-
     private String firstName;
-
     private String lastName;
-
     private String email;
-
     private String username;
-
     private Set<RoleDto> roles;
 
-    public static UserDTO fromEntity(User user) {
+    public static UserDTO mapFromEntity(User user) {
         return new UserDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getUsername(), RoleDto.fromEntitySet(user.getRoles()));
     }
 
-    public static List<UserDTO> fromEntityList(List<User> userList) {
+    public static List<UserDTO> mapFromEntityList(List<User> userList) {
         return userList.stream()
-                .map(UserDTO::fromEntity)
+                .map(UserDTO::mapFromEntity)
                 .collect(Collectors.toList());
     }
-
 }
