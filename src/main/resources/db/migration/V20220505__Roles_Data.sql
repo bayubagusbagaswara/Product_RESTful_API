1be96f1219a5f7e447474c0f171cd2e1a297ca0f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-14 07:39:47 +0700

    update schema roles data

--- src/main/resources/db/migration/V20220505__Roles_Data.sql
+++ src/main/resources/db/migration/V20220505__Roles_Data.sql
@@ -1,5 +1,3 @@
-insert into roles (id, name, created_at, status_record, updated_at)
-values (1, 'MANAGER', current_timestamp, 'ACTIVE', current_timestamp),
-        (2, 'ADMIN', current_timestamp, 'ACTIVE', current_timestamp),
-        (3, 'CUSTOMER', current_timestamp, 'ACTIVE', current_timestamp),
-        (4, 'USER', current_timestamp, 'ACTIVE', current_timestamp);
+insert into roles (name, created_at, status_record, updated_at)
+values ('ADMIN', current_timestamp, 'ACTIVE', current_timestamp),
+        ('USER', current_timestamp, 'ACTIVE', current_timestamp);
