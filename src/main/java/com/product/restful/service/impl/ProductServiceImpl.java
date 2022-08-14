7ac84314aee4a310cb29564dc543e1b7e43c1d3d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-14 19:38:13 +0700

    update all methods ProductServiceImpl

--- src/main/java/com/product/restful/service/impl/ProductServiceImpl.java
+++ src/main/java/com/product/restful/service/impl/ProductServiceImpl.java
@@ -1,10 +1,16 @@
 package com.product.restful.service.impl;
 
-import com.product.restful.dto.product.*;
+import com.product.restful.dto.product.CreateProductRequest;
+import com.product.restful.dto.product.ListProductRequest;
+import com.product.restful.dto.product.ListProductResponse;
+import com.product.restful.dto.product.ProductDTO;
+import com.product.restful.dto.product.UpdateProductRequest;
 import com.product.restful.entity.Product;
 import com.product.restful.exception.ResourceNotFoundException;
+import com.product.restful.mapper.ProductMapper;
 import com.product.restful.repository.ProductRepository;
 import com.product.restful.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,7 +19,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -23,36 +28,36 @@ import java.util.stream.StreamSupport;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository, ProductMapper productMapper) {
         this.productRepository = productRepository;
+        this.productMapper = productMapper;
     }
 
     @Override
     public ProductDTO createProduct(CreateProductRequest createProductRequest) {
-
         Product product = new Product();
         product.setName(createProductRequest.getName());
         product.setPrice(createProductRequest.getPrice());
         product.setQuantity(createProductRequest.getQuantity());
         product.setDescription(createProductRequest.getDescription());
-        product.setCreatedAt(Instant.now());
-
         productRepository.save(product);
-        return ProductDTO.mapFromEntity(product);
+        return productMapper.mapFromProduct(product);
     }
 
     @Override
     public ProductDTO getProductById(String id) {
         Product product = productRepository.getProductById(id);
-        return ProductDTO.mapFromEntity(product);
+        return productMapper.mapFromProduct(product);
     }
 
     @Override
     public List<ProductDTO> getAllProduct() {
         Iterable<Product> products = productRepository.findAll();
         List<Product> productList = StreamSupport.stream(products.spliterator(), false).collect(Collectors.toList());
-        return ProductDTO.mapFromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
@@ -62,7 +67,7 @@ public class ProductServiceImpl implements ProductService {
         product.setPrice(updateProductRequest.getPrice());
         product.setQuantity(updateProductRequest.getQuantity());
         productRepository.save(product);
-        return ProductDTO.mapFromEntity(product);
+        return productMapper.mapFromProduct(product);
     }
 
     @Override
@@ -82,7 +87,7 @@ public class ProductServiceImpl implements ProductService {
         Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
         Page<Product> productPage = productRepository.findAll(pageable);
         List<Product> productList = productPage.getContent();
-        List<ProductDTO> productDTOS = ProductDTO.mapFromEntityList(productList);
+        List<ProductDTO> productDTOS = productMapper.mapFromProductList(productList);
 
         return ListProductResponse.builder()
                 .products(productDTOS)
@@ -97,71 +102,71 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductDTO getProductByName(String name) {
         Product product = productRepository.getProductByName(name);
-        return ProductDTO.mapFromEntity(product);
+        return productMapper.mapFromProduct(product);
     }
 
     @Override
     public List<ProductDTO> getProductByNameContaining(String name) {
         List<Product> productList = productRepository.findByNameContainsIgnoreCase(name);
-        return ProductDTO.mapFromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByNameStartingWith(String name) {
         List<Product> productList = productRepository.findByNameStartingWithIgnoreCase(name);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByNameContainingOrderByName(String name) {
         List<Product> productList = productRepository.findByNameContainsIgnoreCaseOrderByName(name);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByNameContainingOrderByNameDesc(String name) {
         List<Product> productList = productRepository.findByNameContainsIgnoreCaseOrderByNameDesc(name);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByNameContainingAndPriceBetween(String name, BigDecimal priceMin, BigDecimal priceMax) {
         List<Product> productList = productRepository.findByNameContainsIgnoreCaseAndPriceBetween(name, priceMin, priceMax);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByNameContainingOrderByPrice(String name) {
         List<Product> productList = productRepository.findByNameContainsIgnoreCaseOrderByPriceDesc(name);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByNameContainingOrderByPriceDesc(String name) {
         List<Product> productList = productRepository.findByNameContainsIgnoreCaseOrderByPrice(name);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByPriceBetween(BigDecimal priceMin, BigDecimal priceMax) {
         List<Product> productList = productRepository.findByPriceBetween(priceMin, priceMax);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByPriceGreaterThanEqual(BigDecimal price) {
         List<Product> productList = productRepository.findByPriceGreaterThanEqual(price);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
     public List<ProductDTO> getProductByPriceLessThanEqual(BigDecimal price) {
         List<Product> productList = productRepository.findByPriceLessThanEqual(price);
-        return ProductDTO.fromEntityList(productList);
+        return productMapper.mapFromProductList(productList);
     }
 
     @Override
-    public Product getProductByIdNeW(String id) {
+    public Product getProductByIdRawData(String id) {
         return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product", "id", id));
     }
 }
