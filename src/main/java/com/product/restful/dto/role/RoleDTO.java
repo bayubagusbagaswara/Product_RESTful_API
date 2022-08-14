5c8cb96d8502c69a82bcc54fd6abe38e573eee84
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-14 19:26:19 +0700

    update dto RoleDTO

--- src/main/java/com/product/restful/dto/role/RoleDTO.java
+++ src/main/java/com/product/restful/dto/role/RoleDTO.java
@@ -1,13 +1,9 @@
 package com.product.restful.dto.role;
 
-import com.product.restful.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -17,14 +13,4 @@ public class RoleDTO {
 
     private Long id;
     private String name;
-
-    public static RoleDTO mapFromEntity(Role role) {
-        return new RoleDTO(role.getId(), role.getName().name());
-    }
-
-    public static Set<RoleDTO> mapFromEntitiesSet(Set<Role> roles) {
-        return roles.stream()
-                .map(RoleDTO::mapFromEntity)
-                .collect(Collectors.toSet());
-    }
 }
