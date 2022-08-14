4e9f25c0efdb8f6df950a20f60d27a92cc8420fd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-14 19:28:11 +0700

    add RoleMapper

---
+++ src/main/java/com/product/restful/mapper/RoleMapper.java
@@ -0,0 +1,22 @@
+package com.product.restful.mapper;
+
+import com.product.restful.dto.role.RoleDTO;
+import com.product.restful.entity.Role;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class RoleMapper {
+
+    public RoleDTO mapFromRole(Role role) {
+        return new RoleDTO(role.getId(), role.getName().name());
+    }
+
+    public Set<RoleDTO> mapFromEntitiesSet(Set<Role> roles) {
+        return roles.stream()
+                .map(this::mapFromRole)
+                .collect(Collectors.toSet());
+    }
+}
