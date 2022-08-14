3dbad63e09c0170aed148d2931db14c4c615e45b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-14 14:28:36 +0700

    update CreateRoleRequest

--- src/main/java/com/product/restful/dto/role/CreateRoleRequest.java
+++ src/main/java/com/product/restful/dto/role/CreateRoleRequest.java
@@ -1,17 +1,15 @@
 package com.product.restful.dto.role;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RoleRequest {
+public class CreateRoleRequest {
 
     @NotBlank(message = "Role Name must not be blank")
     private String name;
