42e1ccdb12e1b3e841b3f6de99fe19232f656573
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-14 19:28:21 +0700

    add UserMapper

---
+++ src/main/java/com/product/restful/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.product.restful.mapper;
+
+import com.product.restful.dto.role.RoleDTO;
+import com.product.restful.dto.user.UserDTO;
+import com.product.restful.entity.user.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserMapper {
+
+    private final RoleMapper roleMapper;
+
+    public UserMapper(RoleMapper roleMapper) {
+        this.roleMapper = roleMapper;
+    }
+
+    public UserDTO mapFromUser(User user) {
+        return UserDTO.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .roles(roleMapper.mapFromEntitiesSet(user.getRoles()))
+                .build();
+    }
+
+    public List<UserDTO> mapFromUserList(List<User> userList) {
+        return userList.stream()
+                .map(this::mapFromUser)
+                .collect(Collectors.toList());
+    }
+}
