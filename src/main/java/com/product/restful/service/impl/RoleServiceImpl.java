9cd79548be4097dee724246ff0a8e6dc264af062
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-15 09:05:49 +0700

    update method deleteRole

--- src/main/java/com/product/restful/service/impl/RoleServiceImpl.java
+++ src/main/java/com/product/restful/service/impl/RoleServiceImpl.java
@@ -1,17 +1,13 @@
 package com.product.restful.service.impl;
 
-import com.product.restful.dto.MessageResponse;
 import com.product.restful.dto.role.CreateRoleRequest;
 import com.product.restful.dto.role.RoleDTO;
 import com.product.restful.entity.Role;
 import com.product.restful.entity.enumerator.RoleName;
-import com.product.restful.entity.user.UserPrincipal;
-import com.product.restful.exception.AccessDeniedException;
 import com.product.restful.exception.ResourceNotFoundException;
 import com.product.restful.mapper.RoleMapper;
 import com.product.restful.repository.RoleRepository;
 import com.product.restful.service.RoleService;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,15 +32,8 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public MessageResponse deleteRole(Long id, UserPrincipal currentUser) {
-        Role role = roleRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Role", "id", id));
-
-        if (!currentUser.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ADMIN.toString()))) {
-            throw new AccessDeniedException(new MessageResponse(Boolean.FALSE, "You don't have permission to delete role of: " + role.getName()));
-        }
-
+    public void deleteRole(Long id) {
+        Role role = roleRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Role", "id", id));
         roleRepository.delete(role);
-        return new MessageResponse(Boolean.TRUE, "You successfully deleted role id of: " + id);
     }
 }
