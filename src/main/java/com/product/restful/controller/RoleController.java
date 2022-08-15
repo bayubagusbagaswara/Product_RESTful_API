df4a575315169be8a6f846d1f37accae56d83d6d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-15 09:07:16 +0700

    update handler deleteRole

--- src/main/java/com/product/restful/controller/RoleController.java
+++ src/main/java/com/product/restful/controller/RoleController.java
@@ -4,11 +4,11 @@ import com.product.restful.dto.MessageResponse;
 import com.product.restful.dto.WebResponse;
 import com.product.restful.dto.role.CreateRoleRequest;
 import com.product.restful.dto.role.RoleDTO;
-import com.product.restful.entity.user.UserPrincipal;
 import com.product.restful.service.RoleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,15 +21,17 @@ public class RoleController {
         this.roleService = roleService;
     }
 
+    @PreAuthorize(value = "hasAuthority('ADMIN')")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<WebResponse<RoleDTO>> createRole(@RequestBody CreateRoleRequest createRoleRequest) {
         RoleDTO roleDto = roleService.createRole(createRoleRequest);
         return new ResponseEntity<>(new WebResponse<>(Boolean.TRUE, "Role was created successfully", roleDto), HttpStatus.OK);
     }
 
+    @PreAuthorize(value = "hasAuthority('ADMIN')")
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<MessageResponse> deleteRole(@PathVariable(name = "id") Long id, UserPrincipal currentUser) {
-        MessageResponse messageResponse = roleService.deleteRole(id, currentUser);
-        return new ResponseEntity<>(messageResponse, HttpStatus.OK);
+    public ResponseEntity<MessageResponse> deleteRole(@PathVariable(name = "id") Long id) {
+        roleService.deleteRole(id);
+        return new ResponseEntity<>(new MessageResponse(Boolean.TRUE, "You successfully deleted role id of: " + id), HttpStatus.OK);
     }
 }
