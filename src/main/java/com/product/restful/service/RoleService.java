6aa5bd6e17d722845ad91bd7d75fdeb158e8bcdf
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-15 09:03:59 +0700

    update method deleteRole

--- src/main/java/com/product/restful/service/RoleService.java
+++ src/main/java/com/product/restful/service/RoleService.java
@@ -1,13 +1,11 @@
 package com.product.restful.service;
 
-import com.product.restful.dto.MessageResponse;
 import com.product.restful.dto.role.CreateRoleRequest;
 import com.product.restful.dto.role.RoleDTO;
-import com.product.restful.entity.user.UserPrincipal;
 
 public interface RoleService {
 
     RoleDTO createRole(CreateRoleRequest createRoleRequest);
 
-    MessageResponse deleteRole(Long id, UserPrincipal currentUser);
+    void deleteRole(Long id);
 }
