5a055f76a05b0b7512b8f423ba1d9dfecd0c096c
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-11 13:38:22 +0700

    update ProductIntegrationTest

--- src/test/java/com/product/restful/integration/ProductIntegrationTest.java
+++ src/test/java/com/product/restful/integration/ProductIntegrationTest.java
@@ -1,8 +1,16 @@
 package com.product.restful.integration;
 
+import com.product.restful.dto.product.CreateProductRequest;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import static io.restassured.RestAssured.port;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.with;
+
+import java.math.BigDecimal;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -10,33 +18,33 @@ public class ProductIntegrationTest {
 
     @LocalServerPort
     private Integer serverPort;
-//
-//    @BeforeEach
-//    void setUp() {
-//        baseURI = "http://localhost";
-//        port = serverPort;
-//        System.out.println("Port: " + serverPort);
-//    }
-//
-//    @Test
-//    @Order(1)
-//    void createProduct() {
-//        CreateProductRequest createProductRequest = CreateProductRequest.builder()
-//                .name("Test Service")
-//                .price(new BigDecimal("12345000"))
-//                .quantity(345)
-//                .build();
-//        with()
-//                .body(createProductRequest)
-//                .accept(MediaType.APPLICATION_JSON_VALUE)
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .when()
-//                .post("/api/products")
-//                .then()
-//                .statusCode(HttpStatus.OK.value())
-//        ;
-//    }
-//
+
+    @BeforeEach
+    void setUp() {
+        baseURI = "http://localhost";
+        port = serverPort;
+        System.out.println("Port: " + serverPort);
+    }
+
+    @Test
+    @Order(1)
+    void createProduct() {
+        CreateProductRequest createProductRequest = CreateProductRequest.builder()
+                .name("Test Service")
+                .price(new BigDecimal("12345000"))
+                .quantity(345)
+                .build();
+        with()
+                .body(createProductRequest)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/api/products")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+        ;
+    }
+
 //    @Test
 //    @Order(2)
 //    void getProductById() {
