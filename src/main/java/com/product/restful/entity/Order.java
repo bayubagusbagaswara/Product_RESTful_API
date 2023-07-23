e8492cbd780b37a4d31cca5e0ab6737089137ef2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-23 17:07:19 +0700

    add model Order

---
+++ src/main/java/com/product/restful/entity/Order.java
@@ -0,0 +1,27 @@
+package com.product.restful.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal total;
+
+    private Instant orderDate;
+}
