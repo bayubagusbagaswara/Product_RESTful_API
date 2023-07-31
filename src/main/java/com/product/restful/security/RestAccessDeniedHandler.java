9e570e81db5142ae7a940e3485e9be8a16e026b8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-31 20:25:27 +0700

    update method handle

--- src/main/java/com/product/restful/security/RestAccessDeniedHandler.java
+++ src/main/java/com/product/restful/security/RestAccessDeniedHandler.java
@@ -2,6 +2,9 @@ package com.product.restful.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.product.restful.dto.MessageResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,26 +13,22 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 public class RestAccessDeniedHandler implements AccessDeniedHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RestAccessDeniedHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(RestAccessDeniedHandler.class);
 
     @Override
-    public void handle(HttpServletRequest httpServletRequest,
-                       HttpServletResponse httpServletResponse,
-                       AccessDeniedException accessDeniedException) throws IOException {
-
-        LOGGER.error("Responding with access denied error. Message - {}", accessDeniedException.getMessage());
-        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("Responding with access denied error. Message - {}", accessDeniedException.getMessage());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
         MessageResponse body = new MessageResponse(Boolean.FALSE, "Access Denied. You don't have permission to access this resource", HttpStatus.FORBIDDEN);
         final ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(httpServletResponse.getOutputStream(), body);
+        mapper.writeValue(response.getOutputStream(), body);
     }
+
 }
