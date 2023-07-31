7005d276d0bca3b58480a7f914914bb81fd301e8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-31 20:09:41 +0700

    update

--- src/main/java/com/product/restful/security/JwtService.java
+++ src/main/java/com/product/restful/security/JwtService.java
@@ -3,12 +3,12 @@ package com.product.restful.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class JwtService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(JwtService.class);
+    private static final Logger log = LoggerFactory.getLogger(JwtService.class);
 
     @Value(value = "${jwt.secret-key}")
     private String jwtSecretKey;
