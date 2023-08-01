a2e209c8eba0e4a5ad17b1e41b30a98a118d795d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 08:16:10 +0700

    rename package

--- src/main/java/com/product/restful/dto/refreshtoken/RefreshTokenRequest.java
+++ src/main/java/com/product/restful/dto/refreshtoken/RefreshTokenRequest.java
@@ -1,11 +1,10 @@
-package com.product.restful.dto.refreshToken;
+package com.product.restful.dto.refreshtoken;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
