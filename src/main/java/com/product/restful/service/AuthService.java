b9f2fa1e43cd561107f2ffaa82a324a1e1644426
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 08:15:39 +0700

    comment method createRefreshToken

--- src/main/java/com/product/restful/service/AuthService.java
+++ src/main/java/com/product/restful/service/AuthService.java
@@ -1,11 +1,9 @@
 package com.product.restful.service;
 
 import com.product.restful.dto.auth.LogoutRequest;
-import com.product.restful.dto.refreshToken.RefreshTokenRequest;
 import com.product.restful.dto.auth.AuthenticationResponse;
 import com.product.restful.dto.auth.LoginRequest;
 import com.product.restful.dto.auth.RegisterRequest;
-import com.product.restful.dto.user.UserDTO;
 
 public interface AuthService {
 
