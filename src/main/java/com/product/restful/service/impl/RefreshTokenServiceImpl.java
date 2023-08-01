d4167b6321fddc7c8c7084664805d002a9d56a0e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 08:48:58 +0700

    update

--- src/main/java/com/product/restful/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/product/restful/service/impl/RefreshTokenServiceImpl.java
@@ -1,6 +1,6 @@
 package com.product.restful.service.impl;
 
-import com.product.restful.dto.refreshToken.RefreshTokenDTO;
+import com.product.restful.dto.refreshtoken.RefreshTokenDTO;
 import com.product.restful.entity.RefreshToken;
 import com.product.restful.entity.user.User;
 import com.product.restful.exception.RefreshTokenNotFoundException;
@@ -10,45 +10,36 @@ import com.product.restful.mapper.RefreshTokenMapper;
 import com.product.restful.repository.RefreshTokenRepository;
 import com.product.restful.repository.UserRepository;
 import com.product.restful.service.RefreshTokenService;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Service
-@Transactional
+@RequiredArgsConstructor
 public class RefreshTokenServiceImpl implements RefreshTokenService {
 
-    @Value("${app.refreshTokenExpired}")
-    private Long refreshTokenDurationMs;
     private final RefreshTokenRepository refreshTokenRepository;
     private final UserRepository userRepository;
     private final RefreshTokenMapper refreshTokenMapper;
 
-    public RefreshTokenServiceImpl(RefreshTokenRepository refreshTokenRepository, UserRepository userRepository, RefreshTokenMapper refreshTokenMapper) {
-        this.refreshTokenRepository = refreshTokenRepository;
-        this.userRepository = userRepository;
-        this.refreshTokenMapper = refreshTokenMapper;
-    }
-
-    @Override
-    public RefreshTokenDTO generateRefreshToken(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
-
-        if (refreshTokenRepository.findByUserId(userId).isPresent()) {
-            refreshTokenRepository.delete(user.getRefreshToken());
-        }
-
-        RefreshToken refreshToken = new RefreshToken(
-                user, UUID.randomUUID().toString().replace("-",""),
-                Instant.now().plusMillis(refreshTokenDurationMs), Instant.now()
-        );
-
-        refreshTokenRepository.save(refreshToken);
-        return refreshTokenMapper.mapFromRefreshToken(refreshToken);
-    }
+//    @Override
+//    public RefreshTokenDTO generateRefreshToken(Long userId) {
+//        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+//
+//        if (refreshTokenRepository.findByUserId(userId).isPresent()) {
+//            refreshTokenRepository.delete(user.getRefreshToken());
+//        }
+//
+//        RefreshToken refreshToken = new RefreshToken(
+//                user, UUID.randomUUID().toString().replace("-",""),
+//                Instant.now().plusMillis(refreshTokenDurationMs), Instant.now()
+//        );
+//
+//        refreshTokenRepository.save(refreshToken);
+//        return refreshTokenMapper.mapFromRefreshToken(refreshToken);
+//    }
 
     @Override
     public RefreshTokenDTO validateRefreshToken(String refreshToken) {
@@ -58,7 +49,7 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
 
     @Override
     public RefreshToken getRefreshToken(String refreshToken) {
-        return refreshTokenRepository.getByRefreshToken(refreshToken).orElseThrow(() -> new RefreshTokenNotFoundException("Invalid refresh token"));
+        return refreshTokenRepository.getByToken(refreshToken).orElseThrow(() -> new RefreshTokenNotFoundException("Invalid refresh token"));
     }
 
     @Override
@@ -67,7 +58,7 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
 
         if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
             refreshTokenRepository.delete(token);
-            throw new TokenRefreshException(token.getRefreshToken(), "Refresh token was expired. Please make a new login request");
+            throw new TokenRefreshException(token.getToken(), "Refresh token was expired. Please make a new login request");
         }
         return token;
     }
