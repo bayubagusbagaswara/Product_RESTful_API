e2af3ee11f5f3b9c9958bf9e547aca504c8efffe
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 08:14:54 +0700

    comment method generateRefreshToken

--- src/main/java/com/product/restful/service/RefreshTokenService.java
+++ src/main/java/com/product/restful/service/RefreshTokenService.java
@@ -1,11 +1,11 @@
 package com.product.restful.service;
 
-import com.product.restful.dto.refreshToken.RefreshTokenDTO;
+import com.product.restful.dto.refreshtoken.RefreshTokenDTO;
 import com.product.restful.entity.RefreshToken;
 
 public interface RefreshTokenService {
 
-    RefreshTokenDTO generateRefreshToken(Long userId);
+//    RefreshTokenDTO generateRefreshToken(Long userId);
 
     RefreshTokenDTO validateRefreshToken(String refreshToken);
 
