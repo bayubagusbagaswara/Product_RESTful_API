d70dddaae829a8acea937db39f97bfd32e037ac0
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 08:56:41 +0700

    add comment

--- src/main/java/com/product/restful/service/AdminService.java
+++ src/main/java/com/product/restful/service/AdminService.java
@@ -1,4 +1,23 @@
 package com.product.restful.service;
 
 public interface AdminService {
+
+    // create new admin
+
+    // add role admin to other user
+
+    // get admin
+
+    // update admin (just only himself)
+
+    // delete admin or user (for example a user who is no longer active)
+
+    // create new role
+
+    // get role
+
+    // update role
+
+    // delete role
+
 }
