3acb87109a52cc004a93096fa48553af37b09ffa
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 08:48:27 +0700

    update to jakarta

--- src/main/java/com/product/restful/controller/ErrorController.java
+++ src/main/java/com/product/restful/controller/ErrorController.java
@@ -9,6 +9,7 @@ import com.product.restful.exception.RefreshTokenNotFoundException;
 import com.product.restful.exception.ResourceNotFoundException;
 import com.product.restful.exception.TokenRefreshException;
 import com.product.restful.exception.UnauthorizedException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
@@ -19,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
