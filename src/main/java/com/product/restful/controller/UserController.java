0659d4e69371a0d212fda12db7143e8e6355ee31
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-01 08:49:08 +0700

    update to jakarta

--- src/main/java/com/product/restful/controller/UserController.java
+++ src/main/java/com/product/restful/controller/UserController.java
@@ -7,6 +7,7 @@ import com.product.restful.dto.user.CreateUserRequest;
 import com.product.restful.dto.user.UpdateUserRequest;
 import com.product.restful.dto.user.UserDTO;
 import com.product.restful.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,8 +18,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
