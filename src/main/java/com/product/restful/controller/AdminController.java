95be9a0bd5913711314a054387f252108b5eda44
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-05 13:46:36 +0700

    add pre authorize

--- src/main/java/com/product/restful/controller/AdminController.java
+++ src/main/java/com/product/restful/controller/AdminController.java
@@ -4,15 +4,19 @@ import com.product.restful.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/admin")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private static final Logger log = LoggerFactory.getLogger(AdminController.class);
 
     private final AdminService adminService;
+
+    // create new admin
 }
